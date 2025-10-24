b72c4b020a47d681b1d8de41dda97380a9af08cc
lamaisonbrevan <contact@lamaisonbrevan.fr>
2025-10-24 17:33:40 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import useHashRouter from "./router.jsx";
 import Header from "./components/Header.jsx";
 import Home from "./pages/Home.jsx";
 import Gallery from "./pages/Gallery.jsx";
 import Reserve from "./pages/Reserve.jsx";
 
 export default function App() {
-  const { route, goto } = useHashRouter("home");
-
   return (
     <>
-      <Header route={route} goto={goto} />
-      <main>
-        {/* On rend TOUTES les sections; le menu scrolle vers la bonne */}
+      <Header />
+      <main className="pt-[var(--header-height)]">
         <Home />
         <Gallery />
         <Reserve />
