5233e38efcea41a5fa705c0c850b2b1c6ea77fbd
lamaisonbrevan <contact@lamaisonbrevan.fr>
2025-10-24 17:07:19 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import useHashRouter from "./router.jsx";
 import Header from "./components/Header.jsx";
-import Home from "./pages/home.jsx";
-import Gallery from "./pages/gallery.jsx";
-import Reserve from "./pages/reserver.jsx";
+import Home from "./pages/Home.jsx";
+import Gallery from "./pages/Gallery.jsx";
+import Reserve from "./pages/Reserve.jsx";
 
 export default function App() {
   const { route, goto } = useHashRouter("home");
