cacb6b293bd7702cbbd5c3f7e28b79f9c49b2128
lamaisonbrevan <contact@lamaisonbrevan.fr>
2025-10-24 17:22:59 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 // src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";      // tu peux mettre .jsx pour être explicite
-import "./index.css";             // ✅ charge Tailwind et tes styles globaux
+import App from "./App.jsx";
+import "./index.css";              // ⬅️ charge Tailwind + tes styles
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
