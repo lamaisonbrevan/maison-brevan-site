b4f0f8a6fae8512a62b0e737109eda38e296e7eb
lamaisonbrevan <contact@lamaisonbrevan.fr>
2025-10-24 17:02:13 +0200

    Update Gallery.jsx

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,13 +1,7 @@
-import { Container } from "../theme.jsx";
-
 export default function Gallery() {
   return (
-    <section id="galerie" className="pt-24">
-      <Container className="py-12">
-        <h2 className="text-xl md:text-2xl font-serif mb-4">Galerie</h2>
-        <p className="opacity-80 mb-6">Ici on mettra un slider/une grille d’images.</p>
-        {/* place tes images ici */}
-      </Container>
+    <section id="galerie">
+      <h1>Galerie</h1>
     </section>
   );
 }
